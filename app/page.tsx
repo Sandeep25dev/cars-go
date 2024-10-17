4429075595631b8ac2c280b06a7a26fff9d23815
Sandeep25dev <sandeepkumar250903@gmail.com>
2024-10-18 03:56:56 +0530

    started working on the contents section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import React from "react";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -18,6 +23,20 @@ export default function Home() {
             <CustomFilter />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car, i) => (
+                <CarCard key={i} car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results!</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
